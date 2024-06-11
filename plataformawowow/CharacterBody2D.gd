31c8af86116b14c1a492b53fe73cdaddd9f8a09b
preuss <87501978+felipepreuss@users.noreply.github.com>
2024-06-11 12:28:24 -0300

    Add files via upload

--- plataformawowow/CharacterBody2D.gd
+++ plataformawowow/CharacterBody2D.gd
@@ -3,20 +3,22 @@ extends CharacterBody2D
 var jumping = false
 var coyote_time = false
 var jump_buffer = false
+ 
 
 var velocidade = 300
 var gravidade = 10
 var pulo_forca = -300
 
-enum State {
+enum  {
 	IDLE,
 	WALK,
 	JUMP,
 	FALL
 }
+var defaultState = IDLE
 
 func _physics_process(delta):
-	
+	print(coyote_time)
 	velocity.x = Input.get_axis("Esquerda","Direita") * velocidade
 	if velocity.x != 0:
 		$AnimatedSprite2D.flip_h = velocity.x < 0
@@ -27,14 +29,53 @@ func _physics_process(delta):
 		
 	if Input.is_action_just_pressed("Pular") && is_on_floor():
 		velocity.y = pulo_forca
-		 
+		jumping = true
+		defaultState = PULANDO()
+		coyote_time = false
+		
+	#if is_on_floor() and !coyote_time:
+		#	coyote_time = true
+	#elif coyote_time and $Coyote.is_stopped():
+		#$Coyote.start()
+
+		
+	#if coyote_time:
+		#$Coyote.start()
+		
+	#elif Input.is_action_just_pressed("Pular") && !is_on_floor():	
+		#coyote_time = true
+		
 	elif !is_on_floor():
+		
 		$AnimatedSprite2D.play("falling")
 	velocity.y += gravidade
 	move_and_slide()
+	match defaultState:
+		IDLE:
+			PARADO()
+		WALK:
+			ANDANDO()
+		JUMP:
+			PULANDO()
+		FALL:
+			CAINDO()
+			
+func PARADO():
+	pass
+
+func ANDANDO():
+	pass
+
+func PULANDO():
+	pass
 	
+func CAINDO():
+	pass
+
 func _jump_state(delta):
 	if is_on_floor():
 		jumping = false
+		defaultState = FALL
 	else:
 		jumping = true
+		defaultState = JUMP
