6be61308991e03020e9a898d0189b77a6a6332c1
preuss <87501978+felipepreuss@users.noreply.github.com>
2024-06-11 22:34:33 -0300

    Add files via upload

--- plataformawowow/CharacterBody2D.gd
+++ plataformawowow/CharacterBody2D.gd
@@ -27,27 +27,29 @@ func _physics_process(delta):
 	else:
 			$AnimatedSprite2D.play("default")
 		
-	if Input.is_action_just_pressed("Pular") && is_on_floor():
+	if Input.is_action_just_pressed("Pular") && is_on_floor() or coyote_time:
 		velocity.y = pulo_forca
 		jumping = true
 		defaultState = PULANDO()
+		 # Reset coyote time when jumping
+		
+		
+	if is_on_floor() and !coyote_time:
+			coyote_time = false
+			jumping = false
+	
+	if $Coyote.is_stopped():
 		coyote_time = false
+		jumping = false
+	if coyote_time:
+		$Coyote.start()
 		
-	#if is_on_floor() and !coyote_time:
-		#	coyote_time = true
-	#elif coyote_time and $Coyote.is_stopped():
-		#$Coyote.start()
-
-		
-	#if coyote_time:
-		#$Coyote.start()
-		
-	#elif Input.is_action_just_pressed("Pular") && !is_on_floor():	
-		#coyote_time = true
+	elif Input.is_action_just_pressed("Pular") && !is_on_floor():	
+		coyote_time = true
 		
 	elif !is_on_floor():
-		
 		$AnimatedSprite2D.play("falling")
+		$Coyote.stop()  
 	velocity.y += gravidade
 	move_and_slide()
 	match defaultState:
@@ -79,3 +81,7 @@ func _jump_state(delta):
 	else:
 		jumping = true
 		defaultState = JUMP
+
+#func _on_coyote_timeout():
+	#coyote_time = false # Replace with function body.
+	
