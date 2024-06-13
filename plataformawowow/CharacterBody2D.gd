1813909aca4815a93bebdfd906786d0f88858a28
preuss <87501978+felipepreuss@users.noreply.github.com>
2024-06-12 22:44:27 -0300

    Add files via upload

--- plataformawowow/CharacterBody2D.gd
+++ plataformawowow/CharacterBody2D.gd
@@ -30,61 +30,35 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Pular") && (is_on_floor() or coyote_time):
 		velocity.y = pulo_forca
 		jumping = true
-		defaultState = PULANDO()
 		coyote_time = false
-		 # Reset coyote time when jumping
-		
-		
+		$Coyote.stop()
+	
+	move_and_slide()
+	last_floor = is_on_floor()
 	if is_on_floor():
 			coyote_time = false
 			jumping = false
-	
-	 
-	if coyote_time:
-		$Coyote.start()
-		 
-		
-	if !is_on_floor() && !jumping:	
+			$Coyote.stop()
+	velocity.y += gravidade
+	if !is_on_floor()  && !jumping:	
 		coyote_time = true
 		$Coyote.start()
 		$AnimatedSprite2D.play("falling")
+	elif not is_on_floor() and jumping:
+		coyote_time = false
 		
+		$Coyote.stop()	
+		$AnimatedSprite2D.play("falling")
 	elif !is_on_floor():
 		$AnimatedSprite2D.play("falling")
-		 
-	velocity.y += gravidade
-	move_and_slide()
-	last_floor = is_on_floor()
-	match defaultState:
-		IDLE:
-			PARADO()
-		WALK:
-			ANDANDO()
-		JUMP:
-			PULANDO()
-		FALL:
-			CAINDO()
-			
-func PARADO():
-	pass
-
-func ANDANDO():
-	pass
-
-func PULANDO():
-	pass
 	
-func CAINDO():
-	pass
-
-func _jump_state(delta):
-	if is_on_floor():
-		jumping = false
-		defaultState = FALL
-	else:
-		jumping = true
-		defaultState = JUMP
+#func _jump_state(delta):
+#	if is_on_floor():
+	#	jumping = false
+	#	defaultState = FALL
+#	else:
+	#	jumping = true
+		#defaultState = JUMP
 		
 func _on_coyote_timeout():
 	coyote_time = false # Replace with function body.
-	
