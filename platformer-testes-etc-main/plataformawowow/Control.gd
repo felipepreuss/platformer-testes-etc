f646f2c9b818b7c5820773da741ae5c710856db7
preuss <87501978+felipepreuss@users.noreply.github.com>
2024-06-13 16:17:21 -0300

    Delete platformer-testes-etc-main/plataformawowow directory

--- platformer-testes-etc-main/plataformawowow/Control.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_play_pressed():
-	get_tree().change_scene_to_file("res://node_2d.tscn") # Replace with function body.
-
-
-func _on_quit_pressed():
-	get_tree().quit() # Replace with function body.
